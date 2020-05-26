ba898ea060a00012352953ec0542397798a64a78
Prince Okumo <prokum@ttu.ee>
2020-05-26 20:28:29 +0300

    SERVO ADDDED

---
+++ servo_control/servo_control.ino
@@ -0,0 +1,111 @@
+
+#include <LiquidCrystal_I2C.h>
+//Viral Science
+//RFID
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 4 //define green LED pin
+#define LED_R 5 //define red LED
+#define BUZZER 2 //buzzer pin
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+Servo myServo; //define servo name
+
+const int  en = 2, rw = 1, rs = 0, d4 = 4, d5 = 5, d6 = 6, d7 = 7, bl = 3;
+const int i2c_addr = 0x27;
+
+LiquidCrystal_I2C lcd(i2c_addr, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
+
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  myServo.attach(3); //servo pin
+  myServo.write(0); //servo start position
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Scan your Card :");
+
+}
+void loop() 
+{
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "86 41 2A 1F" || content.substring(1)=="DE EE 54 D3"||
+  content.substring(1)=="DB 55 1A 51") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    lcd.setCursor(0,1);
+    lcd.print("Authorized access");
+    Serial.println();
+    delay(500);
+    digitalWrite(LED_G, HIGH);
+    tone(BUZZER, 500);
+    delay(300);
+    noTone(BUZZER);
+    myServo.write(180);
+    delay(5000);
+    myServo.write(0);
+    digitalWrite(LED_G, LOW);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    digitalWrite(LED_R, HIGH);
+    tone(BUZZER, 300);
+    delay(1000);
+    digitalWrite(LED_R, LOW);
+    noTone(BUZZER);
+  }
+} 
+
+void scroll ()
+{lcd.setCursor(0,1);
+  lcd.autoscroll();
+  
+  lcd.print("Authorized denied");
+  /*for(int thechar =0;thechar<10; thechar++)
+  {
+    lcd.print(thechar);
+    //delay(500);
+    }  */
+
+   lcd.noAutoscroll();
+
+   lcd.clear();
+ }
+ 
