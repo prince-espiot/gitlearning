34ae1328777c4df9a80ffd1f4af3a78f31736385
prince-espiot <62214767+prince-espiot@users.noreply.github.com>
2020-05-26 20:40:34 +0300

    Update servo_control.ino

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -16,7 +16,7 @@ Servo myServo; //define servo name
 
 const int  en = 2, rw = 1, rs = 0, d4 = 4, d5 = 5, d6 = 6, d7 = 7, bl = 3;
 const int i2c_addr = 0x27;
-
+//this initialises the LCD screen
 LiquidCrystal_I2C lcd(i2c_addr, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
 
 void setup() 
