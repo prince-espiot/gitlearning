26cdfeb25f04e0ed1ec5c73fcf2664e7127ae846
Prince Okumo <prokum@ttu.ee>
2020-05-26 20:46:46 +0300

    edit

--- servo_control/servo_control.ino
+++ servo_control/servo_control.ino
@@ -18,6 +18,8 @@ const int  en = 2, rw = 1, rs = 0, d4 = 4, d5 = 5, d6 = 6, d7 = 7, bl = 3;
 const int i2c_addr = 0x27;
 
 LiquidCrystal_I2C lcd(i2c_addr, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
+//prince okumo1
+//git get all
 
 void setup() 
 {
